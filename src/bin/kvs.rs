0b183341d4458e542a0b2acda0f5cec11914ed49
seongmin.kim <seongmin.kim@qraftec.com>
2020-01-01 04:01:47 +0900

    clippy & fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,7 +1,6 @@
 extern crate clap;
-use clap::{Arg, App, SubCommand, ArgMatches};
+use clap::{App, Arg, ArgMatches, SubCommand};
 use std::process::exit;
-
 
 fn parse_args() -> ArgMatches<'static> {
     App::new(env!("CARGO_PKG_NAME"))
@@ -12,7 +11,7 @@ fn parse_args() -> ArgMatches<'static> {
             Arg::with_name("version")
                 .short("V")
                 .long("version")
-                .takes_value(false)
+                .takes_value(false),
         )
         .subcommand(
             SubCommand::with_name("get")
@@ -21,62 +20,44 @@ fn parse_args() -> ArgMatches<'static> {
                     Arg::with_name("key")
                         .help("search key")
                         .required(true)
-                        .index(1)
-                )
+                        .index(1),
+                ),
         )
         .subcommand(
             SubCommand::with_name("set")
-                .arg(
-                    Arg::with_name("key")
-                        .required(true)
-                        .index(1)
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .required(true)
-                        .index(2)
-                )
+                .arg(Arg::with_name("key").required(true).index(1))
+                .arg(Arg::with_name("value").required(true).index(2)),
         )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .arg(
-                    Arg::with_name("key")
-                        .required(true)
-                        .index(1)
-                )
-        )
+        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("key").required(true).index(1)))
         .get_matches()
 }
-
 
 fn main() {
     let matches = parse_args();
     if matches.is_present("version") {
         println!(env!("CARGO_PKG_VERSION"))
-    } else {
-        if let Some(subcommand_name) = matches.subcommand_name() {
-            match subcommand_name {
-                "get" => {
-                    eprintln!("unimplemented");
-                    exit(1);
-                },
-                "set" => {
-                    eprintln!("unimplemented");
-                    exit(1);
-                },
-                "rm" => {
-                    eprintln!("unimplemented");
-                    exit(1);
-                },
-                _ => {
-                    eprintln!("available commands, [get, set, rm]");
-                    exit(1);
-                }
+    } else if let Some(subcommand_name) = matches.subcommand_name() {
+        match subcommand_name {
+            "get" => {
+                eprintln!("unimplemented");
+                exit(1);
             }
-        } else {
-            eprintln!("command required, [get, set, rm]");
-            exit(1);
+            "set" => {
+                eprintln!("unimplemented");
+                exit(1);
+            }
+            "rm" => {
+                eprintln!("unimplemented");
+                exit(1);
+            }
+            _ => {
+                eprintln!("available commands, [get, set, rm]");
+                exit(1);
+            }
         }
+    } else {
+        eprintln!("command required, [get, set, rm]");
+        exit(1);
     }
 
     println!("{:?}", matches.is_present("version"));
